4ca3e88a45b979adcf867e3275e34c464600be02
Alvarez Martin Ruben <ruben.alvarezmartin@servexternos.gruposantander.com>
2026-08-06 15:51:55 +0000

    refactor(ios): remove old project paths

--- VeganChecker/Localization.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-func L(_ key: String) -> String {
-    NSLocalizedString(key, comment: "")
-}
-
-func LF(_ key: String, _ arguments: CVarArg...) -> String {
-    String(format: L(key), arguments: arguments)
-}
