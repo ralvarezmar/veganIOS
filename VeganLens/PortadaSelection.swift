b7e15d9285ab79e73b15b11f2f213b7b8274c36e
Alvarez Martin Ruben <ruben.alvarezmartin@servexternos.gruposantander.com>
2026-08-25 11:14:22 +0000

    feat(ios): add twenty portada characters

--- VeganLens/PortadaSelection.swift
+++ VeganLens/PortadaSelection.swift
@@ -2,20 +2,40 @@ import Foundation
 
 let portadaCharacterNames = [
     "apple",
+    "artichoke",
+    "asparagus",
     "banana",
+    "bellpepper",
     "blueberries",
     "broccoli",
     "carrot",
     "chili",
+    "coconut",
     "corn",
+    "kiwi",
+    "kiwislice",
     "leaf",
+    "leek",
+    "melon",
     "mushrooms",
     "orange",
+    "passionfruit",
+    "peach",
     "peas",
+    "pineapple",
     "plum",
+    "pomegranate",
+    "potato",
+    "pumpkin",
+    "radishes",
+    "redcabbage",
+    "redonion",
     "strawberries",
     "strawberry",
-    "tofu"
+    "sweetpotato",
+    "tofu",
+    "watermelon",
+    "zucchini"
 ]
 
 func selectPortadaCharacter(
